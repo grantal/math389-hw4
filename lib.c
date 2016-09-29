32bd41b23097f12396b50009e4acc47b1e3cef14
grantal <AlexV348@gmail.com>
2016-09-29 15:41:50 -0700

    added lib.c

---
+++ lib.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+long mul(long n, long m) {
+    return n*m;
+}
+
+char input_char() {
+    char c;
+    scanf("%c",&c)
+    return c;
+}
+
+void output_long(long v) {
+    printf("%d\n", v);
+}
