79139c6e6ddd52d7267bd62039b9c05a817f916f
grantal <AlexV348@gmail.com>
2016-10-03 20:22:42 -0700

    made addition work

--- my_dc.s
+++ my_dc.s
@@ -1,3 +1,6 @@
+# Alex Grant
+# MAth 389
+
     .globl  my_dc
 	.align 16
 my_dc:
@@ -27,12 +30,22 @@ else:
     cmpq    %rdx, %rcx
     je      print 
 
+    # add top two if it is +
+    movq    $43, %rcx
+    cmpq    %rdx, %rcx
+    je      add 
+
+
     # quit if %rdx is 'x'
     movq    $120, %rcx
     cmpq    %rdx, %rcx
     je      return 
     jmp     loop 
-    jmp     loop 
+
+
+# The "top" of my stack will be at -1(%rbx)
+# This is so I can put the first variable on the stack
+# exactly at %rbx and then increment %rbx
 num:
     # make %rdx its value, not its char value
     subq    $48, %rdx
@@ -46,6 +59,17 @@ print:
     movq    -1(%rbx), %rdi
     callq   output_long
     jmp     loop 
+
+add:
+    movq    -1(%rbx), %rcx   # get top item off stack
+    movq    $0, -1(%rbx)     # overwrite the higher one
+    subq    $1, %rbx         # decrement the calc pointer
+    movq    -1(%rbx), %rdx   # get top item off stack
+    # put the sum at the new top
+    addq    %rcx, %rdx
+    movq    %rdx, -1(%rbx) 
+    jmp     loop 
+    
 
 return:
     movq    $0, %rax    # return 0
