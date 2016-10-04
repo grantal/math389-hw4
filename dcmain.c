6e0bf12a4f1ef4f30c8694b6dc8a93416dfb5961
grantal <AlexV348@gmail.com>
2016-10-03 19:48:55 -0700

    made stack work

--- dcmain.c
+++ dcmain.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 #define BIG_ENOUGH 100
 
 void my_dc(long *calc_stack);
 
 int main() {
-    my_dc((long *)malloc(BIG_ENOUGH*sizeof(long)));
+    long *calc_stack = (long *)malloc(BIG_ENOUGH*sizeof(long)); 
+    //printf("calc stack address: %ld", (long)calc_stack);
+    my_dc(calc_stack);
 }
--- my_dc.s
+++ my_dc.s
@@ -3,16 +3,51 @@
 my_dc:
     pushq   %rbp       
     movq    %rsp, %rbp	
+    movq    %rdi, %rbx # rbx will have the calculator stack pointer
     
+loop:
     # get input
     callq    input_char 
     movq     %rax, %rdx 
 
-    # print what was inputed 
-    pushq %rdx
-    callq output_long
-    popq  %rdx
+    # if rdx is between 48 and 57,
+    # it is a digit
+    movq    $48, %rcx
+    cmp     %rcx, %rdx
+    jl      else
+    movq    $57, %rcx
+    cmp     %rcx, %rdx
+    jg      else
+    
+    jmp     num
 
+else:
+    # print top of stack if it is p
+    movq    $112, %rcx
+    cmpq    %rdx, %rcx
+    je      print 
+
+    # quit if %rdx is 'x'
+    movq    $120, %rcx
+    cmpq    %rdx, %rcx
+    je      return 
+    jmp     loop 
+    jmp     loop 
+num:
+    # make %rdx its value, not its char value
+    subq    $48, %rdx
+    # put %rdx on calc stack
+    movq    %rdx, 0(%rbx) 
+    addq    $1, %rbx
+    jmp     loop 
+
+print:
+    # put top of stack in rdi
+    movq    -1(%rbx), %rdi
+    callq   output_long
+    jmp     loop 
+
+return:
     movq    $0, %rax    # return 0
     popq    %rbp        #
     retq                #
