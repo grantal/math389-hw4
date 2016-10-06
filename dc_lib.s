e076713557ab4d1a876cee2b881a06f6cf3c80ac
grantal <AlexV348@gmail.com>
2016-10-06 10:36:09 -0700

    add test_dc_lib and dc_lib

---
+++ dc_lib.s
@@ -0,0 +1,111 @@
+
+# mod(n,d)
+# rdi % rsi
+# where %rdi is n, the dividend
+# and %rsi is d the divisor
+.globl mod
+mod:
+    # dealing with a negative n
+    movq    $0, %rcx
+    cmpq    $0, %rdi
+    jge      modloop
+    negq    %rdi
+    # putting a flag in rcx
+    movq    $1, %rcx
+modloop:
+    subq    %rsi, %rdi 
+    cmpq    $0, %rdi
+    jl      modreturn
+    jmp     modloop
+modreturn:
+    # how far away -rdi is from rsi
+    # rax = rsi + rdi
+    movq    $0, %rax
+    addq    %rsi, %rax
+    addq    %rdi, %rax
+    cmpq    $1, %rcx  # seeing if the flag is set
+    je      modnegreturn
+    retq
+modnegreturn:
+    negq    %rax
+    retq
+    
+
+.globl divide
+divide: #this is here because c gets confused about calling "div"
+div:
+    # dealing with a negative n
+    movq    $1, %rcx
+    cmpq    $0, %rdi
+    jge     divcallhelp
+    negq    %rdi
+    # putting a flag in rcx
+    movq    $1, %rcx
+divcallhelp:
+    callq   divhelper
+    cmpq    $1, %rcx  # seeing if the flag is set
+    je      divnegreturn
+    retq
+divnegreturn:
+    negq    %rax
+    retq
+    
+divhelper:
+    subq    %rsi, %rdi   # rdi = rsi - rdi
+    # if rsi goes into rdi once, the the quotient is 0
+    cmpq    $0, %rdi     
+    jl      divbasecase
+    # otherwise its the quotient of one less rsi plus 1
+    callq   divhelper
+    addq    $1, %rax
+    retq
+divbasecase:
+    movq    $0, %rax
+    retq
+
+
+# dcon
+# gets decimals
+# rdi has the stack pointer
+.globl dcon
+dcon:
+                          # rdi will point to the element we want
+    #movq    %rdi, %rdx
+    movq    0(%rdi), %rcx # rcx will be the counter 
+    movq    $0, %rax      # rax will be the result
+dcon_loop:
+    cmpq    $0, %rcx      # see if counter is 0
+    jle     dcon_return
+    subq    $1, %rcx      # decrement counter and pointer
+    subq    $8, %rdi
+    # multiply rax by 10
+    # (2*2 + 1)*2 = 10
+    movq    %rax, %rdx
+    salq    %rax
+    salq    %rax
+    addq    %rdx, %rax
+    salq    %rax
+    #add the digit to rax
+    addq    0(%rdi), %rax 
+    jmp     dcon_loop
+dcon_return:
+    retq
+
+# pow
+# computes x^p
+.globl pow
+pow:
+    movq    %rdi, %rdx # rdx is x
+    movq    %rsi, %rcx # rcx is p 
+    movq    $1, %rax   # result
+pow_loop: 
+    cmpq    $0, %rcx
+    jle     pow_return
+    subq    $1, %rcx
+    movq    %rdx, %rdi
+    movq    %rax, %rsi
+    callq   mul        #multiply rax by rdx
+    jmp pow_loop
+pow_return:
+    retq
+
--- test_dc_lib.c
+++ test_dc_lib.c
@@ -3,27 +3,33 @@
 #define BIG_ENOUGH 100
 
 // compile with:
-// gcc -o dc dcmain.c lib.c my_dc.s dc_lib.s -g
+// gcc -o dc test_dc_lib.c lib.c dc_lib.s -g
 
 
 void my_dc(long *calc_stack);
 
 
 int main() {
-    /*
     long a = -13;
     long b = 4;
     printf("%ld div %ld: %ld\n", a, b, (long)(divide(a,b)));
     printf("%ld div %ld: %ld\n", a, b, (long)(a / b));
-    long x = 4l;
-    long p = 2l;
-    printf("%ld to the %ld: %ld\n", x, p, (long)(pow(a,b)));
-    */
     long *calc_stack = (long *)malloc(BIG_ENOUGH*sizeof(long)); 
     calc_stack[0] = 5l;
     calc_stack[1] = 7l;
     calc_stack[2] = 2l;
     printf("dcon: %ld%ld\n", calc_stack[1], calc_stack[0]);
     printf("dcon: %ld\n", (long)dcon(&calc_stack[2])); 
-    my_dc(calc_stack);
+    calc_stack[0] = 6l;
+    calc_stack[1] = 8l;
+    calc_stack[2] = 4l;
+    calc_stack[3] = 5l;
+    calc_stack[4] = 4l;
+    printf("dcon: %ld%ld%ld%ld\n", calc_stack[3], calc_stack[2], calc_stack[1], calc_stack[0]);
+    printf("dcon: %ld\n", (long)dcon(&calc_stack[4])); 
+    /*
+    long x = 4l;
+    long p = 2l;
+    printf("%ld to the %ld: %ld\n", x, p, (long)(pow(a,b)));
+    */
 }
