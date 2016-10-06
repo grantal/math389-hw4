cc7e50cdae61c70a501863e3096687df6942f613
grantal <AlexV348@gmail.com>
2016-10-06 10:58:10 -0700

    removed test code from dcmain

--- dcmain.c
+++ dcmain.c
@@ -10,20 +10,6 @@ void my_dc(long *calc_stack);
 
 
 int main() {
-    /*
-    long a = -13;
-    long b = 4;
-    printf("%ld div %ld: %ld\n", a, b, (long)(divide(a,b)));
-    printf("%ld div %ld: %ld\n", a, b, (long)(a / b));
-    long x = 4l;
-    long p = 2l;
-    printf("%ld to the %ld: %ld\n", x, p, (long)(pow(a,b)));
-    */
     long *calc_stack = (long *)malloc(BIG_ENOUGH*sizeof(long)); 
-    calc_stack[0] = 5l;
-    calc_stack[1] = 7l;
-    calc_stack[2] = 2l;
-    printf("dcon: %ld%ld\n", calc_stack[1], calc_stack[0]);
-    printf("dcon: %ld\n", (long)dcon(&calc_stack[2])); 
     my_dc(calc_stack);
 }
